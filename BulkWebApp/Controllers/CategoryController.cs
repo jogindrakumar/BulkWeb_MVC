dbe8a3d8e417c42725ac435a9ae9a21fb5105b2b
jogindrakumar <joginder9868@outlook.com>
2023-08-16 20:45:14 +0530

    Get-all-category

--- BulkWebApp/Controllers/CategoryController.cs
+++ BulkWebApp/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkWebApp.Data;
+using BulkWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkWebApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
