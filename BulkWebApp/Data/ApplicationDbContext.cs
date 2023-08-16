c6899e50846cd690e84f67fb4fdca4c01c6560f8
jogindrakumar <joginder9868@outlook.com>
2023-08-16 20:38:15 +0530

    Seed Category Table

--- BulkWebApp/Data/ApplicationDbContext.cs
+++ BulkWebApp/Data/ApplicationDbContext.cs
@@ -10,5 +10,16 @@ namespace BulkWebApp.Data
                 
         }
         public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                
+                new Category { Id=1, Name ="Action",DisplayOrder=1},
+                new Category { Id=2, Name = "Comedy",DisplayOrder=2},
+                new Category { Id=3, Name="History",DisplayOrder=3}
+                
+                );
+        }
     }
 }
