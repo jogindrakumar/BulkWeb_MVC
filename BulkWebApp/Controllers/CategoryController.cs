98ae14cb10b299bde018e7ba556de6f0e8b2aa77
jogindrakumar <joginder9868@outlook.com>
2023-08-16 20:58:32 +0530

    Display-all-categories

--- BulkWebApp/Controllers/CategoryController.cs
+++ BulkWebApp/Controllers/CategoryController.cs
@@ -14,7 +14,7 @@ namespace BulkWebApp.Controllers
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
-            return View();
+            return View(objCategoryList);
         }
     }
 }
