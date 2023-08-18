ede44ea2cd10bd714bce97cf45f7f5ce07efe94e
jogindrakumar <joginder9868@outlook.com>
2023-08-18 05:30:09 +0530

    Create-Category-UI

--- BulkWebApp/Controllers/CategoryController.cs
+++ BulkWebApp/Controllers/CategoryController.cs
@@ -16,5 +16,8 @@ namespace BulkWebApp.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        public IActionResult Create() {
+            return View();
+        }
     }
 }
