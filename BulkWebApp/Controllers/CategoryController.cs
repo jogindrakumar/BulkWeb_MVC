f8717abb426385e55da8108b4665886544ec306b
jogindrakumar <joginder9868@outlook.com>
2023-08-19 18:32:35 +0530

    Add-toastr-notification

--- BulkWebApp/Controllers/CategoryController.cs
+++ BulkWebApp/Controllers/CategoryController.cs
@@ -30,7 +30,7 @@ namespace BulkWebApp.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
-                TempData["Success"] = "Category Created Successfully";
+                TempData["success"] = "Category Created Successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -63,7 +63,7 @@ namespace BulkWebApp.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
-                TempData["Success"] = "Category Edited Successfully";
+                TempData["success"] = "Category Edited Successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -97,7 +97,7 @@ namespace BulkWebApp.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
-            TempData["Danger"] = "Category Deleted Successfully";
+            TempData["success"] = "Category Deleted Successfully";
             return RedirectToAction("Index");
 
 
