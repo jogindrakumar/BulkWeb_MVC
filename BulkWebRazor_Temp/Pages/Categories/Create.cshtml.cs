5ac65755470a4c3b5a758f1ff49fbee2fe062e4e
jogindrakumar <joginder9868@outlook.com>
2023-08-20 12:35:03 +0530

    create-category-UI

---
+++ BulkWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,20 @@
+using BulkWebRazor_Temp.Data;
+using BulkWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkWebRazor_Temp.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
