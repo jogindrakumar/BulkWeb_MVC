1489bbef958ad0139a8345b354fc5858b80ad43b
jogindrakumar <joginder9868@outlook.com>
2023-08-20 12:44:08 +0530

    create-category-post-handler

--- BulkWebRazor_Temp/Pages/Categories/Create.cshtml.cs
+++ BulkWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -5,9 +5,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BulkWebRazor_Temp.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
         private readonly ApplicationDbContext _db;
+       
         public Category Category { get; set; }
         public CreateModel(ApplicationDbContext db)
         {
@@ -15,6 +17,11 @@ namespace BulkWebRazor_Temp.Pages.Categories
         }
         public void OnGet()
         {
+        }
+        public IActionResult OnPost(Category obj) {
+        _db.Categories.Add(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
         }
     }
 }
